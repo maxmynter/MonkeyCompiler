4a48a9d1bcf8e0260eada70d4dd964b9f0532d78
Max Mynter <maxmynter@me.com>
2025-04-19 23:54:17 +0200

    Begin AST implementation

---
+++ ast/src/ast.rs
@@ -0,0 +1,25 @@
+pub trait Node {
+    fn token_literal(&self) -> String;
+}
+
+pub trait Statement: Node {
+    fn statement_node(&self) {}
+}
+
+pub trait Expression: Node {
+    fn expression_node(&self) {}
+}
+
+pub struct Program {
+    pub statements: Vec<Box<dyn Statement>>,
+}
+
+impl Node for Program {
+    fn token_literal(&self) -> String {
+        if self.statements.len() > 0 {
+            self.statements[0].token_literal()
+        } else {
+            String::new()
+        }
+    }
+}
