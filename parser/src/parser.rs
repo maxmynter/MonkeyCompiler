81ffbf96ecdc4f3e695d2bd74f927309f1ba69a3
Max Mynter <maxmynter@me.com>
2025-04-22 16:23:08 +0200

    Extend parser

--- parser/src/parser.rs
+++ parser/src/parser.rs
@@ -1,4 +1,4 @@
-use ast::Program;
+use ast::{Identifier, Node, Program, Statement};
 use lexer::{Lexer, Token};
 
 struct Parser<'a> {
@@ -20,5 +20,60 @@ impl<'a> Parser<'a> {
 
     fn parse_program(&mut self) -> Program {
         todo!()
+    }
+}
+
+#[test]
+fn test_let_parsing() {
+    let input = "let x = 5;
+let y = 10;
+let foobar = 838383;
+";
+    let l = Lexer::new(input);
+    let mut p = Parser::new(l);
+    let program = p.parse_program();
+
+    assert_eq!(program.statements.len(), 3);
+
+    let expected_identifiers = ["x", "y", "foobar"];
+
+    for (i, tt) in expected_identifiers.iter().enumerate() {
+        let stmt = &program.statements[i];
+        assert!(
+            test_let_stmt(stmt, tt),
+            "Let statemtent parsing failed: {}",
+            i
+        );
+    }
+}
+
+fn test_let_stmt(stmt: &ast::Statement, name: &str) -> bool {
+    if let Statement::Let(let_stmt) = stmt {
+        if let_stmt.token_literal() != "let" {
+            println!(
+                "statement token literal not `let`, got={}",
+                let_stmt.token_literal()
+            );
+            return false;
+        }
+        if let_stmt.name.value != name {
+            println!(
+                "let_stmt.name.token_literal not {}, got={}",
+                name,
+                let_stmt.name.token_literal()
+            );
+            return false;
+        }
+        if let_stmt.name.token_literal() != name {
+            println!(
+                "let_stmt.name.token_literal not `{}`, got={}",
+                name,
+                let_stmt.name.token_literal()
+            );
+        }
+        true
+    } else {
+        println!("Statement is not a LetStatement");
+        false
     }
 }
