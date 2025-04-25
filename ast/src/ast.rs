1bf6e2d41bb85ecb55dd1c83e91045d3b774cd54
Max Mynter <maxmynter@me.com>
2025-04-25 17:01:40 +0200

    AST prep for Parsing

--- ast/src/ast.rs
+++ ast/src/ast.rs
@@ -1,4 +1,4 @@
-use lexer::Token;
+use lexer::{Token, TokenType};
 
 pub enum Statement {
     Let(LetStatement),
@@ -8,6 +8,7 @@ pub enum Statement {
 
 pub trait Node {
     fn token_literal(&self) -> String;
+    fn as_string(&self) -> String;
 }
 
 pub struct Program {
@@ -22,6 +23,14 @@ impl Node for Program {
             String::new()
         }
     }
+
+    fn as_string(&self) -> String {
+        let mut out = String::new();
+        for stmt in &self.statements {
+            out.push_str(&stmt.as_string())
+        }
+        out
+    }
 }
 
 impl Node for Statement {
@@ -32,18 +41,65 @@ impl Node for Statement {
             Statement::Expression(expression_stmt) => expression_stmt.token.literal.clone(),
         }
     }
+
+    fn as_string(&self) -> String {
+        let mut out = String::new();
+        match self {
+            Statement::Let(stmt) => {
+                out.push_str(&stmt.token.literal);
+                out.push_str(" ");
+                out.push_str(&stmt.name.value);
+                out.push_str(" = ");
+                if let Some(expr) = &stmt.value {
+                    out.push_str(&expr.as_string());
+                }
+                out.push_str(";");
+            }
+            Statement::Return(return_stmt) => {
+                out.push_str(&return_stmt.token.literal);
+                out.push_str(" ");
+                if let Some(ret_val) = &return_stmt.value {
+                    out.push_str(&ret_val.as_string());
+                }
+                out.push_str(";");
+            }
+            Statement::Expression(expr_stmt) => {
+                if let Some(expression) = &expr_stmt.value {
+                    out.push_str(&expression.as_string());
+                }
+            }
+        }
+        out
+    }
 }
 
 impl Node for Expression {
     fn token_literal(&self) -> String {
-        let Expression::Identifier(ident) = &self;
-        ident.token_literal()
+        match self {
+            Expression::Identifier(ident) => ident.token_literal(),
+            Expression::Statement(expr_statement) => expr_statement.token.literal.clone(),
+        }
+    }
+    fn as_string(&self) -> String {
+        match self {
+            Expression::Identifier(Identifier { value, .. }) => value.to_string(),
+            Expression::Statement(ExpressionStatement { value, .. }) => {
+                if let Some(expression) = value {
+                    expression.as_string()
+                } else {
+                    String::new()
+                }
+            }
+        }
     }
 }
 
 impl Node for Identifier {
     fn token_literal(&self) -> String {
         self.token.literal.clone()
+    }
+    fn as_string(&self) -> String {
+        self.value.to_string()
     }
 }
 
@@ -70,4 +126,32 @@ pub struct ExpressionStatement {
 
 pub enum Expression {
     Identifier(Identifier),
+    Statement(ExpressionStatement),
+}
+
+#[test]
+fn test_ast() {
+    let prog = Program {
+        statements: vec![Statement::Let(LetStatement {
+            token: Token {
+                kind: TokenType::LET,
+                literal: "let".to_string(),
+            },
+            name: Identifier {
+                token: Token {
+                    kind: TokenType::IDENT,
+                    literal: "myVar".to_string(),
+                },
+                value: "myVar".to_string(),
+            },
+            value: Some(Box::new(Expression::Identifier(Identifier {
+                token: Token {
+                    kind: TokenType::IDENT,
+                    literal: "anotherVar".to_string(),
+                },
+                value: "anotherVar".to_string(),
+            }))),
+        })],
+    };
+    assert_eq!(prog.as_string(), "let myVar = anotherVar;");
 }
