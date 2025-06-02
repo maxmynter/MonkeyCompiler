16fa231469c23bb53132b1311db13a03aa2731bf
Max Mynter <maxmynter@me.com>
2025-06-02 22:49:39 +0200

    (refactor) Object type

--- object/src/object.rs
+++ object/src/object.rs
@@ -1,48 +1,18 @@
+use ast::Expression;
+
 pub enum ObjectType {
-    IntegerObj,
-    BooleanObj,
-    NullObj,
+    Integer { value: i64 },
+    Boolean { value: bool },
+    Null,
 }
 
-pub struct Integer {
-    value: i64,
-}
-
-pub struct Boolean {
-    value: bool,
-}
-
-pub struct Null {}
-
-trait Inspect {
-    fn inspect(&self) -> String;
-}
-
-trait TypedObject {
-    fn object_type(&self) -> ObjectType;
-}
-
-impl Inspect for Integer {
-    fn inspect(&self) -> String {
-        self.value.to_string()
-    }
-}
-
-impl TypedObject for Integer {
-    fn object_type(&self) -> ObjectType {
-        ObjectType::IntegerObj
-    }
-}
-
-impl Inspect for Boolean {
-    fn inspect(&self) -> String {
-        self.value.to_string()
-    }
-}
-
-impl TypedObject for Boolean {
-    fn object_type(&self) -> ObjectType {
-        ObjectType::BooleanObj
+impl ObjectType {
+    pub fn inspect(&self) -> String {
+        match self {
+            ObjectType::Null => "null".to_string(),
+            ObjectType::Boolean { value } => value.to_string(),
+            ObjectType::Integer { value } => value.to_string(),
+        }
     }
 }
 
