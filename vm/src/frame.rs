b1bd8bfe615629647aeb46078c66679868f5ec76
Max Mynter <maxmynter@me.com>
2025-08-27 15:32:05 +0200

    Frame now takes closures

--- vm/src/frame.rs
+++ vm/src/frame.rs
@@ -3,20 +3,32 @@ use object::Object;
 
 #[derive(Debug, Clone)]
 pub struct Frame {
-    pub instructions: Instruction,
     pub ip: isize,
     pub base_pointer: usize,
+    pub cl: Object,
 }
 
 impl Frame {
-    pub fn new(func: Object, base_pointer: usize) -> Self {
-        match func {
-            Object::CompiledFunction { instructions, .. } => Frame {
-                instructions,
+    pub fn new(cl: Object, base_pointer: usize) -> Self {
+        match &cl {
+            Object::Closure { .. } => Frame {
                 ip: -1,
                 base_pointer,
+                cl,
             },
-            _ => panic!("Can only create a new stack frame for a CompiledFunction object"),
+            _ => panic!("new frame needs closure object"),
+        }
+    }
+    pub fn instructions(&self) -> &Instruction {
+        match &self.cl {
+            Object::Closure { func, .. } => {
+                if let Object::CompiledFunction { instructions, .. } = func.as_ref() {
+                    &instructions
+                } else {
+                    panic!("closure function should have instructions")
+                }
+            }
+            _ => panic!("closure should be a compiled function"),
         }
     }
 }
