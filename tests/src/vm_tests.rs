9fa36c6cfe39b5b78ca6deb0ba18bdd3fb3c59da
Max Mynter <maxmynter@me.com>
2025-09-11 19:17:10 +0200

    (tests) More complex closure test cases

--- tests/src/vm_tests.rs
+++ tests/src/vm_tests.rs
@@ -777,6 +777,41 @@ let adder = newAdder(1, 2);
 adder(8);",
             expected: Ok(Object::Integer { value: 11 }),
         },
+        VmTestCase {
+            input: "let newAdderOuter = fn(a, b) {
+let c = a + b;
+fn(d) {
+let e = d + c;
+fn(f) { e + f; };
+};
+};
+let newAdderInner = newAdderOuter(1, 2)
+let adder = newAdderInner(3);
+adder(8);",
+            expected: Ok(Object::Integer { value: 14 }),
+        },
+        VmTestCase {
+            input: "let a = 1;
+let newAdderOuter = fn(b) {
+fn(c) {
+fn(d) { a + b + c + d };
+};
+};
+let newAdderInner = newAdderOuter(2)
+let adder = newAdderInner(3);
+adder(8);",
+            expected: Ok(Object::Integer { value: 14 }),
+        },
+        VmTestCase {
+            input: "let newClosure = fn(a, b) {
+let one = fn() { a; };
+let two = fn() { b; };
+fn() { one() + two(); };
+};
+let closure = newClosure(9, 90);
+closure();",
+            expected: Ok(Object::Integer { value: 99 }),
+        },
     ];
     run_vm_tests(tests);
 }
